c84467aa4057221e2ce7474c0a0d179554f76f5b
SrikanthBethi9885 <srikanth.bethi@missiong3.com>
2025-04-04 18:05:40 +0530

    added my workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,11 @@ import js from "@eslint/js";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: { ...globals.node } } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] }
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["eslint:recommended", "prettier"],
+  },
 ]);
